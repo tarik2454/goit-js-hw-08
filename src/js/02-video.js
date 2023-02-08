ea6c49ed0f577a25c2a6b56ceb1b6c4d1cc650eb
Taras Kovalyk <tarik2454@gmail.com>
2023-02-08 11:45:24 +0100

    Adds solutions to task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player', {
+  id: 19231868,
+  width: 640,
+});
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+
+const getCurrentTime = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+
+player
+  .setCurrentTime(getCurrentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
